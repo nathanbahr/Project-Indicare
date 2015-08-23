a28a17d5cbc8551b8ed0d00b37d8390b2e666e91
nathanbahr <nbgameboy@gmail.com>
2015-08-22 23:15:01 -0700

    More Results
    Pages will now display links from the 5th and 6th columns.

--- result.php
+++ result.php
@@ -3,8 +3,10 @@ while($row = $query->fetch()) {
 	echo '<p id="' . $row['Item'] . '"><b>' . $row['Item'] . '</b> ' . $row['Definition'] . '&nbsp' . $row['History'] 
 	. '&nbsp' . ' <a href="' . $row['Link1'] . '">' . $row['Link1DN'] . '</a>' 
 	. '&nbsp' . ' <a href="' . $row['Link2'] . '">' . $row['Link2DN'] .  '</a>' 
-	. '&nbsp' . ' <a href="' . $row['Link3'] . '">' . $row['Link3DN'] .  '</a>' 
-	. '&nbsp' . ' <a href="' . $row['Link4'] . '">' . $row['Link4DN'] .  '</a>' 
+	. '&nbsp' . ' <a href="' . $row['Link3'] . '">' . $row['Link3DN'] .  '</a>'
+	. '&nbsp' . ' <a href="' . $row['Link4'] . '">' . $row['Link4DN'] .  '</a>'
+	. '&nbsp' . ' <a href="' . $row['Link5'] . '">' . $row['Link5DN'] .  '</a>' 
+	. '&nbsp' . ' <a href="' . $row['Link6'] . '">' . $row['Link6DN'] .  '</a>' 	
 	. '</p>';
 }
 ?>
