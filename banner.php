c1940181056a29115bade238f901658771b8a76e
nathanbahr <nbgameboy@gmail.com>
2015-12-30 21:41:47 -0800

    title fonts and hyperlink
    Changed the font of the page title. Added a hyperlink back to the home
    page from the "Project Indicāre" text.

--- banner.php
+++ banner.php
@@ -1,7 +1,9 @@
 <div class="headerLeft">
 
 	<div class="search">
-		<h2>Project Indicāre [BETA]</a></h2> 
+		<div id="search">
+			<h1><a href="index.html">Project Indicāre [BETA]</a></h1>
+		</div>
 
 		<?php
 		$input = array("Latin. To show, to point out, to indicate. <a href='https://en.wiktionary.org/wiki/indicare'>Wikipedia</a>", "The online index.", "A quick reference guide for computers and technology", "You can't know what you don't know you don't know.", "No");
