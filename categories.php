a960f0768318d148a199ff98a281ecb6eba63619
nathan <nathanbahr@me.com>
2019-04-06 14:40:35 -0700

    Create categories.php

---
+++ categories.php
@@ -0,0 +1,36 @@
+<?php include_once("head.php") ?>
+	<title>categories - TOCRG</title>
+</head>
+	<body>
+		<?php include_once("analyticstracking.php") ?>
+		<?php include_once("dbconnect.php") ?>
+		<?php include_once("banner.php") ?>
+		<div class="content-narrow">
+			<?php include_once("title.php") ?>
+			<ul>
+				<li><a href="commands.php">commands</a></li>
+				<li><a href="companies.php">companies</a></li>
+				<li><a href="conferences.php">conferences</a></li>	
+				<li><a href="devices.php">devices</a></li>
+				<li><a href="errors.php">errors</a></li>
+				<li><a href="electrical.php">electrical</a></li>
+				<li><a href="games.php">games</a></li>
+				<li><a href="hardware.php">hardware</a></li>
+				<li><a href="html.php">html</a></li>
+				<li><a href="networking.php">networking</a></li>
+				<li><a href="operating-systems.php">operating systems</a></li>
+				<li><a href="people.php">people</a></li>
+				<li><a href="place.php">place</a></li>
+				<li><a href="product.php">product</a></li>
+				<li><a href="programming.php">programming</a></li>
+				<li><a href="security.php">security</a></li>
+				<li><a href="servers.php">servers</a></li>
+				<li><a href="software.php">software</a></li>
+				<li><a href="space.php">space</a></li>	
+				<li><a href="webdev.php">webdev</a></li>
+				<li><a href="websites.php">websites</a></li>
+			</ul>
+		</div>
+		<?php include_once("footer.php") ?>
+	</body>
+</html>
