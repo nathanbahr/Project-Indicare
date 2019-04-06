96c1da22f76c9ca87ec4de98bad7c6998141ac2b
nathan <nathanbahr@me.com>
2019-04-06 14:13:21 -0700

    Updated a.php with result count

--- a.php
+++ a.php
@@ -1,27 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<?php include_once("head.php") ?>
+<?php include_once("head.php"); ?>
+	<title>A - TOCRG</title>
 </head>
 <body>
-	<?php include_once("analyticstracking.php") ?>
-	<?php include_once("dbconnect.php") ?>
-	<?php include_once("banner.php") ?>
+	<?php include_once("analyticstracking.php"); ?>
+	<?php include_once("dbconnect.php"); ?>
+	<?php include_once("banner.php"); ?>
 <div class="content">
-<?php include_once("title.php") ?>
+	<?php include_once("title.php"); ?>
 	<div id="col1">
 			<?php
-				include ("query1.php");	
-				include ("result.php");	
+				include ("query1.php");
+				include ("query2.php");
+				$queryOneCount = $queryOne->rowCount();
+				$queryTwoCount = $queryTwo->rowCount();
+				print $queryOneCount + $queryTwoCount. " results";
+				include ("result1.php");	
 			?>
 	</div>
 	<div id="col2">
 			<?php
-				include ("query2.php");	
-				include ("result.php");	
+				include ("result2.php");	
 			?>
 	</div>
 </div>
-<?php include_once("footer.php") ?>
+<?php include_once("footer.php"); ?>
 </body>
 </html>
