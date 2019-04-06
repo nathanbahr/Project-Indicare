dbb05231ea40f6acea9e12bf5508f2c145b6e7e9
nathan <nathanbahr@me.com>
2019-04-06 14:38:03 -0700

    removed redundant inks in comment

--- links.php
+++ links.php
@@ -1,22 +1,11 @@
-
 <div class="links">
 	<div class="list-centered">
 		<ul id="nav">
 			<li><a href="num.php">#</a></li><li><a href="a.php">A</a> </li><li> <a href="b.php">B</a> </li><li> <a href="c.php">C</a> </li><li> <a href="d.php">D</a> </li><li> <a href="e.php">E</a> </li><li> <a href="f.php">F</a> </li><li> <a href="g.php">G</a> </li><li> <a href="h.php">H</a> </li><li> <a href="i.php">I</a> </li><li> <a href="j.php">J</a> </li><li> <a href="k.php">K</a> </li><li> <a href="l.php">L</a> </li><li> <a href="m.php">M</a> </li><li> <a href="n.php">N</a> </li><li> <a href="o.php">O</a> </li><li> <a href="p.php">P</a> </li><li> <a href="q.php">Q</a> </li><li> <a href="r.php">R</a> </li><li> <a href="s.php">S</a> </li><li> <a href="t.php">T</a> </li><li> <a href="u.php">U</a> </li><li> <a href="v.php">V</a> </li><li> <a href="w.php">W</a> </li><li> <a href="x.php">X</a> </li><li> <a href="y.php">Y</a> </li><li> <a href="z.php">Z</a></li>
 		</ul>
 	</div>
-	<p><a href="#end">End</a></p>
 </div>
 
-
-
-
-
-
-
-
-
-
-<!--
-<a href="num.php">#</a> | <a href="a.php">A</a> | <a href="b.php">B</a> | <a href="c.php">C</a> | <a href="d.php">D</a> | <a href="e.php">E</a> | <a href="f.php">F</a> | <a href="g.php">G</a> | <a href="h.php">H</a> | <a href="i.php">I</a> | <a href="j.php">J</a> | <a href="k.php">K</a> | <a href="l.php">L</a> | <a href="m.php">M</a> | <a href="n.php">N</a> | <a href="o.php">O</a> | <a href="p.php">P</a> | <a href="q.php">Q</a> | <a href="r.php">R</a> | <a href="s.php">S</a> | <a href="t.php">T</a> | <a href="u.php">U</a> | <a href="v.php">V</a> | <a href="w.php">W</a> | <a href="x.php">X</a> | <a href="y.php">Y</a> | <a href="z.php">Z</a>
--->
+<div class="page-jump-link">
+	<p><a href="#end">End</a></p>
+</div>
