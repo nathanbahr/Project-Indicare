73c859031274877bbaf98d2e3df3d351a67fa8cc
nathan <nathanbahr@me.com>
2019-04-06 14:30:11 -0700

    Create devices.php

---
+++ devices.php
@@ -0,0 +1,23 @@
+<?php include_once("head.php") ?>
+	<title>devices - TOCRG</title>
+</head>
+<body>
+	<?php include_once("analyticstracking.php") ?>
+	<?php include_once("dbconnect.php") ?>
+	<?php include_once("banner.php") ?>
+<div class="content-narrow">
+	<?php include_once("title.php") ?>
+	<?php
+		$query = $handler->query(
+			"SELECT * 
+			FROM `ALL` 
+			WHERE `Type ID` =108
+			ORDER BY `Item` ASC");
+				
+			include ("result.php");	
+		?>
+	</div>
+</div>
+<?php include_once("footer.php") ?>
+</body>
+</html>
