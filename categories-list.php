59add958ab99f2b497b78d6efac07a05beaed7a6
nathan <nathanbahr@me.com>
2019-04-06 14:45:44 -0700

    Create categories-list.php

---
+++ categories-list.php
@@ -0,0 +1,24 @@
+<ul>
+    <li><a href="databases.php">databases</a></li>
+    <li><a href="commands.php">commands</a></li>
+    <li><a href="companies.php">companies</a></li>
+    <li><a href="conferences.php">conferences</a></li>	
+    <li><a href="devices.php">devices</a></li>
+    <li><a href="errors.php">errors</a></li>
+    <li><a href="electrical.php">electrical</a></li>
+    <li><a href="games.php">games</a></li>
+    <li><a href="hardware.php">hardware</a></li>
+    <li><a href="html.php">html</a></li>
+    <li><a href="networking.php">networking</a></li>
+    <li><a href="operating-systems.php">operating systems</a></li>
+    <li><a href="people.php">people</a></li>
+    <li><a href="place.php">place</a></li>
+    <li><a href="product.php">product</a></li>
+    <li><a href="programming.php">programming</a></li>
+    <li><a href="security.php">security</a></li>
+    <li><a href="servers.php">servers</a></li>
+    <li><a href="software.php">software</a></li>
+    <li><a href="space.php">space</a></li>	
+    <li><a href="webdev.php">webdev</a></li>
+    <li><a href="websites.php">websites</a></li>
+</ul>
